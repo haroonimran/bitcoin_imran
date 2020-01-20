f641b9a95114dda46f6f0407d4b82b0ebbe6e538
imran <haroon.imran@gmail.com>
2020-01-19 19:37:41 -0500

    updated t03

--- code/t03_convert_private_key_WIF.py
+++ code/t03_convert_private_key_WIF.py
@@ -1,11 +1,16 @@
 #example 4-5
 
 import bitcoin
+import hashlib
+
+
+#hashlib enables us to utilze the SHA256 function
+from hashlib import sha256
 
 #generate a random private key
 valid_private_key = False
 while not valid_private_key:
-    #generate a random private key in Hex
+    #generate a random private key in Hex - this is a string datatype (STR)
     new_private_key_hex = bitcoin.random_key()
     
     #convert the hex key to decimal
@@ -31,6 +36,23 @@ print("The private key in the WIF format = ",new_private_key_hex_WIF)
 #Prefix 0x80 for mainnet or 0xef for test net. The following step generates a Hex literal string with "80" prefixed to it:
 new_private_key_hex01 = str(80)+new_private_key_hex
 print("Step1-WIF: New_private_key_hex01 with 80 prefixed: ", new_private_key_hex01)
-print("new_private_key_hex is of type ",type(new_private_key_hex))
-print("new_private_key_hex01 is of type ", type(new_private_key_hex01))
-print("private_key_decimal is of type",type(private_key_decimal))
+
+#Step 2: Perform 2 SHA256 hashes on the above private key
+new_private_key_bytes_SHA256 = sha256(sha256(new_private_key_hex01.encode()).digest()).digest()
+print("the double sha 256 of the pre-fixed Private Key is:",new_private_key_bytes_SHA256)
+
+new_private_key_HEX_SHA256 = new_private_key_bytes_SHA256.hex()
+print("the double sha 256 of the pre-fixed Private Key is:",new_private_key_HEX_SHA256)
+
+#new_private_key_hex01_SHA256 = sha256(sha256(new_private_key_hex01.encode('utf-8')).digest()).digest()
+#print("the double sha 256 of the pre-fixed Private Key is:",new_private_key_hex01_SHA256)
+
+#Step 3: Get the first 4 bytes of the double sha Hex:
+first4 = new_private_key_HEX_SHA256[0:8]
+print("First 4 bytes = ",first4)
+
+#Step 4: Add checksum to priv key (suffix)
+new_private_key_hex01_with_checksum = new_private_key_hex01 + first4
+print("New private key w checksum = ",new_private_key_hex01_with_checksum)
+
+#Step 5: Base58 Check Encode
---
+++ code/temp_t03_convert_private_key_WIF.py
@@ -0,0 +1,11 @@
+#example 4-5
+
+
+from bitcoin import base58
+
+
+
+private_key_base58 = base58(new_private_key_hex01_with_checksum)
+print("private_key_base58 =", private_key_base58)
+
+
