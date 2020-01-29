7ee8253b1dc9e8d2e57fa4bce356e6c44db61658
imran <haroon.imran@gmail.com>
2020-01-29 00:20:12 -0500

    ecch.py updated

--- code/t06_generate_public_key_ECC.py
+++ code/t06_generate_public_key_ECC.py
@@ -4,13 +4,25 @@
 # Setting up a FiniteField of numbers
 ##############################################################################################
 
-import ecc
-from ecc import Point
+import ecch
+from ecch import Point
+from ecch import FieldElement
 
 
-point1 = Point(-1,-1,5,7)
-print(point1)
-point2 = Point(-1,1,5,7)
-print(point2)
-point3 = point1+point2
-print(point3)
+#Chapter 3
+#Exercise 1
+# Evaluate whether the following points are on the curve y**2 = x**3 + 7 over F(223)
+#(192,105) (17,56) (200,119) (1,193) (42,99)
+
+#instantiate points
+
+x = FieldElement(192,223)
+y = FieldElement(105,223)
+a = FieldElement(0,223)
+b = FieldElement(7,223)
+
+
+p1 = Point(x,y,a,b)
+print(p1.x.prime)
+
+#print(p1)
